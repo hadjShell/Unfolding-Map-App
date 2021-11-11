4e0375e5aa0c301b6096166c518514186f8c5628
Jiayuan Zhang <c0057833@ncl.ac.uk>
2021-11-11 00:34:33 +0000

    Color changed based on time

--- src/myDemo/MyPAppletDemo.java
+++ src/myDemo/MyPAppletDemo.java
@@ -15,7 +15,20 @@ public class MyPAppletDemo extends PApplet{
         // dynamic proportionally resize
         backgroundImg.resize(0, height);
         image(backgroundImg, 0, 0);
-        fill(255, 209, 0);
+        // color-changed sun
+        int[] sunRGB = sunColor(second());
+        fill(sunRGB[0], sunRGB[1], sunRGB[2]);
         ellipse(width/5, height/5, 50, 50);
+    }
+
+    // 30 secs yellow 0 sec black
+    private int[] sunColor(int second) {
+        int[] rgb = new int[3];
+        float ratio = 1 - Math.abs(second - 30) / 30.0f;
+        rgb[0] = (int) (255 * ratio);
+        rgb[1] = (int) (255 * ratio);
+        rgb[2] = 0;
+
+        return rgb;
     }
 }
